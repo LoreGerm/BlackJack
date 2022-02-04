c68f609974e2d6167c8f37b1343cb9ad133e7e3d
LoreGerm <96469569+LoreGerm@users.noreply.github.com>
2022-02-04 21:12:24 +0100

    Add files via upload

--- classi/carte.py
+++ classi/carte.py
@@ -2,24 +2,22 @@ from random import random
 import random
 
 class Mazzo:
-    mazzo_carte = []
+    __mazzo_carte = []
     def __init__(self, numero_carte):
         for i in range (1,numero_carte + 1, 1):
-            self.mazzo_carte.append(Quadri(i))
-            self.mazzo_carte.append(Picche(i))
-            self.mazzo_carte.append(Cuori(i))
-            self.mazzo_carte.append(Fiori(i))
+            self.__mazzo_carte.append(Quadri(i))
+            self.__mazzo_carte.append(Picche(i))
+            self.__mazzo_carte.append(Cuori(i))
+            self.__mazzo_carte.append(Fiori(i))
+
+        random.shuffle(self.__mazzo_carte)
 
     def get_Mazzo(self):
-        return self.mazzo_carte
+        return self.__mazzo_carte
 
     @classmethod
-    def estrai(self):
-        l_mazzo = len(self.mazzo_carte) - 1
-        l_inizio = 0
-        random.shuffle(self.mazzo_carte)
-        num_estratto = random.randint(l_inizio, l_mazzo)
-        return self.mazzo_carte.pop(num_estratto)
+    def estrai(cls):
+        return cls.__mazzo_carte.pop(len(cls.__mazzo_carte)-1)
 
 class Carta:
     __seme = ""
@@ -31,31 +29,74 @@ class Carta:
     def __str__(self):
         return str(self.numero) + " di " + self.seme
 
+    def get_numero(self):
+        return self.numero
+
 
 class Quadri(Carta):
 
     def __init__(self, numero):
-        self.seme = "quadri"
-        self.numero = numero
+        if numero == 11:
+            self.seme = "Quadri"
+            self.numero = 'J'
+        elif numero == 12:
+            self.seme = "Quadri"
+            self.numero = 'Q'
+        elif numero == 13:
+            self.seme = "Quadri"
+            self.numero = 'K'
+        else:
+            self.seme = "Quadri"
+            self.numero = numero
     
     
 
 class Fiori(Carta):
 
     def __init__(self, numero):
-        self.seme = "Fiori"
-        self.numero = numero
+        if numero == 11:
+            self.seme = "Fiori"
+            self.numero = 'J'
+        elif numero == 12:
+            self.seme = "Fiori"
+            self.numero = 'Q'
+        elif numero == 13:
+            self.seme = "Fiori"
+            self.numero = 'K'
+        else:
+            self.seme = "Fiori"
+            self.numero = numero
 
 
 class Picche(Carta):
 
     def __init__(self, numero):
-        self.seme = "Picche"
-        self.numero = numero
+        if numero == 11:
+            self.seme = "Picche"
+            self.numero = 'J'
+        elif numero == 12:
+            self.seme = "Picche"
+            self.numero = 'Q'
+        elif numero == 13:
+            self.seme = "Picche"
+            self.numero = 'K'
+        else:
+            self.seme = "Picche"
+            self.numero = numero
 
 
 class Cuori(Carta):
 
     def __init__(self, numero):
-        self.seme = "Cuori"
-        self.numero = numero
+        if numero == 11:
+            self.seme = "Cuori"
+            self.numero = 'J'
+        elif numero == 12:
+            self.seme = "Cuori"
+            self.numero = 'Q'
+        elif numero == 13:
+            self.seme = "Cuori"
+            self.numero = 'K'
+        else:
+            self.seme = "Cuori"
+            self.numero = numero
--- main.py
+++ main.py
@@ -1,9 +1,49 @@
 
 from classi.banco import banco
-from classi.carte import *
+from classi.giocatore import giocatore
 
 
 
-b = banco(13)
-print(str(b.estrai()))
 
+
+p1 = giocatore('Ciatteo',50000)
+
+player = [p1]
+
+b = banco(13,player)
+
+print('Soldi totali: ', p1.get_soldi())         # VERIFICARE
+scommessa = int(input('Soldi da puntare: '))
+p1.scommetti(scommessa)
+
+
+b.distribuisci(player)
+
+
+
+print(p1.get_nome(),' ', p1.str_carte(), 'totale: ',p1.get_totale())
+print('---------------------------------')
+print('Banco: ', b.get_carte()[1])
+print('---------------------------------')
+
+p1.turno_giocatore()
+
+b.turno_banco()
+print('carte bnco: ', b.str_carte(), 'totale: ',b.get_totale())
+
+print(b.confronto())
+print(p1.get_soldi())
+
+
+# CREA LISTA GIOCAORI   OK
+# CREA BANCO    OK
+
+# :INIZIO GIOCO
+
+# SCOMMETTI     OK
+# DISTIBUISCI 2 CARTE   OK
+# TURNO GIOCATORE E BANCO (CHIEDE CARTA(CHECK SBALLO O BJ), STARE, RADDOPPIO)   OK
+# CONFRONTO CON BANCO   OK
+# RISULTATO SCOMMESSA   OK
+
+# GOTO INIZIO
