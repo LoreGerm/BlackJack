59561fe885ebf463b42d9f9aefa77c1afc7e3e86
P4na <vip_P4na@protonmail.com>
2022-02-14 19:14:55 +0100

    non funge il thread

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
-import json
+
+import time 
 import zmq
+import multiprocessing as mp
 
 from classi.banco import Banco
 from classi.giocatore import Giocatore
@@ -7,28 +9,55 @@ from classi.tavolo import Tavolo
 
 # CRIPTARE I DATI CON AES
 
-class server:
+class Server:
 
-    context = zmq.Context()
-    socket = context.socket(zmq.REP)
-    socket.bind("tcp://192.168.200.70:5555")
-    b = Banco(13)
-    players = []
+    __context = zmq.Context()
+    __socket = __context.socket(zmq.REP)
+    __socket.bind("tcp://192.168.1.159:5555")
+    __b = Banco(13)
+    __players = []
+    __fine = False
 
+    def tempo(self):
+        n = 1
+        a = time.time()
+        b = a + 15
+        while a < b:
+            a = time.time()
+            print(a)
+        self.__fine = True
+        
+    def ricevi(self):
+        for i in range(20):
+            print('ciao')
+        
+    def __init__(self):
+        
+        t = mp.Process(target = self.tempo())
+        t2 = mp.Process(target=self.ricevi())
+        t.start()
+        t2.start()
+        
+        t.join()
+        t2.join()
+        
+        
+        t = Tavolo(self.__b, self.__players)
+        print(self.__players)
     
-
-    while True:
         
-        p = socket.recv_string()  # SERVER RICEVE I GIOCATORI DAL CLIENT
-        player = Giocatore(p,50000)
-        soldi = str(player.get_soldi())
-        players.append(player)
-        socket.send_string(soldi)
-        t = Tavolo(b, players)
-        print(players)
+if __name__ == "__main__":
+    s = Server()
+    
+'''
+while self.__fine == False:
+            p = self.__socket.recv_string()  # SERVER RICEVE I GIOCATORI DAL CLIENT
+            player = Giocatore(p, 50000)
+            soldi = str(player.get_soldi())
+            self.__players.append(player)
+            self.__socket.send_string(soldi)
         
-
-        #b = banco(13,players)
-
+'''
+        
 
     
